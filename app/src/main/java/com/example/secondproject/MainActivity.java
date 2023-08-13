127013cc42eea8a8af3e850d2aaacbb02b886f57
Dell <talhaf952@gmail.com>
2023-08-13 11:43:17 -0800

    Button is clicked

--- app/src/main/java/com/example/secondproject/MainActivity.java
+++ app/src/main/java/com/example/secondproject/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.secondproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.LoginFilter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+//    EditText LoginText,PasswordText;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button is Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-}
+};

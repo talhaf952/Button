89a4127eaadee23ce64d478143f177113c003d89
Dell <talhaf952@gmail.com>
2023-08-13 12:31:14 -0800

    second activity

---
+++ app/src/main/java/com/example/secondproject/secondactivity.java
@@ -0,0 +1,15 @@
+package com.example.secondproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class secondactivity extends AppCompatActivity {
+
+    Button ClickButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondactivity);
+    }
+}
